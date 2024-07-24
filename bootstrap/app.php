c4a991fbb2e23b796be724cdd6edc8aa9fc2d31e
Luis-F-Oliveira <luis.f.oliveira505141@outlook.com>
2024-07-23 20:32:49 -0400

    auth and protected routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,8 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->statefulApi();
+
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \App\Http\Middleware\Authenticate::class,
         ]);
 
         $middleware->alias([
